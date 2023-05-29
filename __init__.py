13e02be11c1d5aef447230898621a265b079e87f
Axel3012 <axel.root3012@gmail.com>
2023-05-28 21:12:28 -0600

    Cargamos nuestro documento de manejo de Sqlite para consultas y solicitudes

---
+++ __init__.py
@@ -0,0 +1,2 @@
+RUTA_EXCEL = 'data/data_prueba_tecnica.xlsx'
+RUTA_DB = 'data/data_prueba_tecnica.db'
--- section_1.py
+++ section_1.py
@@ -1,3 +1,12 @@
 import sqlite3
 import pandas
 
+from . import RUTA_DB, RUTA_EXCEL
+
+df_excel_to_db = pandas.read_excel(RUTA_EXCEL, sheet_name='data_prueba_1')
+
+conexion = sqlite3.connect(RUTA_DB)
+df_excel_to_db.to_sql('data_prueba_1', conexion, if_exists='replace', index=False)
+
+conexion.close()
+
